562ae345e92e090a40c3f9c09d11a7dcb8b3273c
Manas Chopra <74074241+manas95826@users.noreply.github.com>
2023-10-03 19:38:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,16 @@
-from dotenv import load_dotenv
-import os
 import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.huggingface import HuggingFaceEmbeddings
-from langchain.vectorstores import FAISS #facebook AI similarity search
+from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain import HuggingFaceHub
 
-
 def main():
-    load_dotenv()
     st.set_page_config(page_title="Ask your PDF")
     st.header("Ask Your PDF")
 
-    pdf = st.file_uploader("Upload your pdf",type="pdf")
+    pdf = st.file_uploader("Upload your pdf", type="pdf")
 
     if pdf is not None:
         pdf_reader = PdfReader(pdf)
@@ -22,7 +18,7 @@ def main():
         for page in pdf_reader.pages:
             text += page.extract_text()
 
-        # spilit ito chuncks
+        # Split it into chunks
         text_splitter = CharacterTextSplitter(
             separator="\n",
             chunk_size=1000,
@@ -31,24 +27,26 @@ def main():
         )
         chunks = text_splitter.split_text(text)
 
-        # create embedding
+        # Create embeddings
         embeddings = HuggingFaceEmbeddings()
 
-        knowledge_base = FAISS.from_texts(chunks,embeddings)
+        knowledge_base = FAISS.from_texts(chunks, embeddings)
 
-        user_question = st.text_input("Ask Question about your PDF:")
+        user_question = st.text_input("Ask a Question about your PDF:")
         if user_question:
+            # Hardcode the Hugging Face Hub API token here (not recommended for security)
+            huggingfacehub_api_token = "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"
+
             docs = knowledge_base.similarity_search(user_question)
-            llm = HuggingFaceHub(repo_id="google/flan-t5-large", model_kwargs={"temperature":5,
-                                                      "max_length":64})
-            chain = load_qa_chain(llm,chain_type="stuff")
-            response = chain.run(input_documents=docs,question=user_question)
+            llm = HuggingFaceHub(
+                repo_id="google/flan-t5-large",
+                model_kwargs={"temperature": 5, "max_length": 64},
+                huggingface_api_token=huggingfacehub_api_token
+            )
+            chain = load_qa_chain(llm, chain_type="stuff")
+            response = chain.run(input_documents=docs, question=user_question)
 
             st.write(response)
-
-
-
-        # st.write(chunks)
 
 if __name__ == '__main__':
     main()
